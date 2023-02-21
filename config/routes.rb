5ae55d6c9c96e858dbb115bb299e803789fc9189
yoshiki_murakami <eiim.yoshiki@gmail.com>
2023-02-21 19:39:28 +1100

    Add devise custom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    # ↓ ローカルに追加されたコントローラーを参照する(コントローラー名: "コントローラーの参照先")
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
 end
